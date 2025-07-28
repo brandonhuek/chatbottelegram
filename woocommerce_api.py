00572edc57082ad66f6ae48f9d53fcdca9b38ddb
InnovationCrafters <cannabisinnovationcbd@gmail.com>
2025-07-28 20:24:54 +0200

    Update woocommerce_api.py

--- woocommerce_api.py
+++ woocommerce_api.py
@@ -1,4 +1,3 @@
-
 import requests
 import random
 import os
@@ -7,6 +6,10 @@ def get_products():
     url = os.environ.get("WOOCMERCE_URL")
     ck = os.environ.get("WOOCMERCE_CK")
     cs = os.environ.get("WOOCMERCE_CS")
+
+    # Verificar si la URL es válida
+    if not url or not url.startswith(('http://', 'https://')):
+        raise ValueError("La URL de WooCommerce no está configurada correctamente. Asegúrate de incluir 'http://' o 'https://'.")
 
     response = requests.get(f"{url}/wp-json/wc/v3/products", auth=(ck, cs))
     if response.status_code == 200:
